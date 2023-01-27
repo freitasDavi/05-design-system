61864575d8f26e482bc60d13802e221e1c381063
Davi <davi.frrs@gmail.com>
2023-01-27 19:19:33 -0300

    box component

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -37,87 +37,12 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
-  Button: () => Button
+  Box: () => Box
 });
 module.exports = __toCommonJS(src_exports);
 
 // src/styles/index.ts
 var import_react = require("@stitches/react");
-
-// ../tokens/dist/index.mjs
-var colors = {
-  white: "#FFF",
-  black: "#000",
-  gray100: "#E1E1E6",
-  gray200: "#A9A9B2",
-  gray400: "#7C7C8A",
-  gray500: "#505059",
-  gray600: "#323238",
-  gray700: "#29292E",
-  gray800: "#202024",
-  gray900: "#121214",
-  ignite300: "#00B37E",
-  ignite500: "#00875F",
-  ignite700: "#015F43",
-  ignite900: "#00291D"
-};
-var fonts = {
-  default: "Roboto, sans-serif",
-  code: "monospace"
-};
-var fontSizes = {
-  xxs: "0.625rem",
-  xs: "0.75rem",
-  sm: "0.875rem",
-  md: "1rem",
-  lg: "1.125rem",
-  xl: "1.25rem",
-  "2xl": "1.5rem",
-  "4xl": "2rem",
-  "5xl": "2.25rem",
-  "6xl": "3rem",
-  "7xl": "4rem",
-  "8xl": "4.5rem",
-  "9xl": "6rem"
-};
-var fontWeights = {
-  regular: "400",
-  medium: "500",
-  bold: "700"
-};
-var lineHeights = {
-  shorter: "125%",
-  short: "140%",
-  base: "160%",
-  tall: "180%"
-};
-var radii = {
-  px: "1px",
-  xs: "4px",
-  sm: "6px",
-  md: "8px",
-  lg: "16px",
-  full: "99999px"
-};
-var space = {
-  1: "0.25rem",
-  2: "0.5rem",
-  3: "0.75rem",
-  4: "1rem",
-  5: "1.25rem",
-  6: "1.5rem",
-  7: "1.75rem",
-  8: "2rem",
-  10: "2.5rem",
-  12: "3rem",
-  16: "4rem",
-  20: "5rem",
-  40: "10rem",
-  64: "16rem",
-  80: "20rem"
-};
-
-// src/styles/index.ts
 var {
   styled,
   css,
@@ -133,41 +58,24 @@ var {
     width: "space"
   }),
   theme: {
-    colors,
-    fontSizes,
-    fontWeights,
-    fonts,
-    lineHeights,
-    radii,
-    space
+    colors: void 0,
+    fontSizes: void 0,
+    fontWeights: void 0,
+    fonts: void 0,
+    lineHeights: void 0,
+    radii: void 0,
+    space: void 0
   }
 });
 
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite300",
+// src/components/Box.tsx
+var Box = styled("div", {
+  padding: "$4",
   borderRadius: "$md",
-  fontWeight: "bold",
-  color: "$white",
-  border: 0,
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4"
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
+  backgroundColor: "$gray800",
+  border: "1px solid $gray600"
 });
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  Button
+  Box
 });
