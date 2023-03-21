10cbb3e4d162c3f5a0ce59ca9796f659345ad9ca
Davi <davi.frrs@outlook.com>
2023-03-21 20:07:14 -0300

    fix: teste

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,3 +1,5 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
@@ -8,12 +10,17 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
-    }
-
-    return config
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Use the same "resolve" configuration as your app
+      // resolve: (await import('../vite.config.js')).default.resolve,
+      // Add dependencies to pre-optimization
+      base: "/05-design-system/",
+      // optimizeDeps: {
+      //   include: ['storybook-dark-mode'],
+      // },
+    });
   },
   docs: {
     autodocs: true
